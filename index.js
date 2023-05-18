a1c4c03f8ab5a66167bc05c336797e25675be4c2
AlinaKa7 <7k.alina32@gmail.com>
2023-05-18 17:57:36 +0300

    Corrected

--- index.js
+++ index.js
@@ -1,146 +1,48 @@
+seasonList = ['winter', 'summer'];
+categoryList = ['vegetables', 'fruits'];
 vegetables = ['cabbage', 'avocado', 'tomato'];
 fruits = ['grapes', 'raspberry', 'coconut'];
+vegetablesPrice = [8, 30, 10];
+fruitsPrice = [20, 25, 50];
 
 finalPrice = 0;
 
 do {
-    season = prompt('In which season would you like to purchase: winter or summer?'); 
+    season = prompt('In which season would you like to purchase: winter or summer?').toLowerCase().replaceAll(' ', '') ; 
+} while (!seasonList.includes(season));
 
-    do {
-        category = prompt('Choose category in which you want to buy products: vegetables or fruits');
-
-        if (category.toLowerCase().replaceAll(' ', '') === 'vegetables') {
-            product = prompt('Choose product from category of vagetables: cabbage, avocado, tomato');
-        } else if (category.toLowerCase().replaceAll(' ', '') === 'fruits') {
-            product = prompt('Choose product from category of vagetables: grapes, raspberry, coconut');
-        } else {
-            product = alert('Try again');
-        }
-
-        do {
-            count = parseInt(prompt('Enter count of product'));
-        } while (count < 1 || isNaN(count));
-
-        roughPrice = 0;
-
-        switch (product.toLowerCase().replaceAll(' ', '')) {
-            case 'cabbage':
-                price = 8;
-                roughPrice = price * count;
-                break;
-            case 'avocado':
-                price = 30;
-                roughPrice = price * count;
-                break;  
-            case 'tomato':
-                price = 10;
-                roughPrice = price * count;
-                break;
-            case 'grapes':
-                price = 20;
-                roughPrice = price * count;
-                break;
-            case 'raspberry':
-                price = 25;
-                roughPrice = price * count;
-                break;
-            case 'coconut':
-                price = 50;
-                roughPrice = price * count;
-                break;
-        }
-
-    } while (category.toLowerCase().replaceAll(' ', '') !== 'vegetables' && category.toLowerCase().replaceAll(' ', '') !== 'fruits');
-
-} while (season.toLowerCase().replaceAll(' ', '') 
-!== 'winter' && season.toLowerCase().replaceAll(' ', '') !== 'summer');
-
-if (season.toLowerCase().replaceAll(' ', '') === 'winter') {
-    finalPrice = roughPrice * 2;
-} else if (season.toLowerCase().replaceAll(' ', '') === 'summer') {
-    finalPrice = roughPrice * 0.8;
+if (season === 'winter') {
+    multiplier = 2;
 } else {
-    finalPrice = 0;
+    multiplier = 0.8;
 }
 
-imageCabbage = document.createElement("img");
-imageCabbage.src = "images/vegetables/cabbage.svg";
-imageCabbage.width = "100";
-imageCabbage.height = "100";
-imageCabbage.alt = 'cabbage';
+do {
+    category = prompt('Choose category in which you want to buy products: vegetables or fruits').toLowerCase().replaceAll(' ', '');
 
-divCabbage = document.createElement('div');
-divCabbage.align = "center";
-divCabbage.append(imageCabbage);
+    if (category === 'vegetables') {
+        product = prompt('Choose product from category of vagetables: cabbage, avocado, tomato').toLowerCase().replaceAll(' ', '');
+        vegetablesType = vegetables.indexOf(product);
+        priceOfProduct = vegetablesPrice[vegetablesType];
+    } else {
+        product = prompt('Choose product from category of vagetables: grapes, raspberry, coconut').toLowerCase().replaceAll(' ', '');
+        fruitsType = fruits.indexOf(product);
+        priceOfProduct = fruitsPrice[fruitsType];
+    }
 
-imageAvocado = document.createElement("img");
-imageAvocado.src = "images/vegetables/avocado.svg";
-imageAvocado.width = "100";
-imageAvocado.height = "100";
-imageAvocado.alt = 'avocado';
+} while (!categoryList.includes(category));
 
-divAvocado = document.createElement('div');
-divAvocado.align = "center";
-divAvocado.append(imageAvocado);
+do {
+    count = parseInt(prompt('Enter count of product').replaceAll(' ', ''));
+} while (count <= 1 || isNaN(count));
 
-imageTomato = document.createElement("img");
-imageTomato.src = "images/vegetables/tomato.svg";
-imageTomato.width = "100";
-imageTomato.height = "100";
-imageTomato.alt = 'tomato';
+finalPrice = priceOfProduct * count * multiplier;
 
-divTomato = document.createElement('div');
-divTomato.align = "center";
-divTomato.append(imageTomato);
+productImg = `<img src="images/${category}/${product}.svg" alt="${product}" width="100" height="100" />`;
 
-imageCoconut = document.createElement("img");
-imageCoconut.src = "images/fruits/coconut.svg";
-imageCoconut.width = "100";
-imageCoconut.height = "100";
-imageCoconut.alt = 'coconut';
-
-divCoconut = document.createElement('div');
-divCoconut.align = "center";
-divCoconut.append(imageCoconut);
-
-imageRaspberry = document.createElement("img");
-imageRaspberry.src = "images/fruits/raspberry.svg";
-imageRaspberry.width = "100";
-imageRaspberry.height = "100";
-imageRaspberry.alt = 'raspberry';
-
-divRaspberry = document.createElement('div');
-divRaspberry.align = "center";
-divRaspberry.append(imageRaspberry);
-
-imageGrapes = document.createElement("img");
-imageGrapes.src = "images/fruits/grapes.svg";
-imageGrapes.width = "100";
-imageGrapes.height = "100";
-imageGrapes.alt = 'coconut';
-
-divGrapes = document.createElement('div');
-divGrapes.align = "center";
-divGrapes.append(imageGrapes);
-
-
-if (product === 'cabbage') {
-    insertImage = divCabbage;
-} else if (product === 'avocado') {
-    insertImage = divAvocado;
-} else if (product === 'tomato') {
-    insertImage = divTomato;
-} else if (product === 'grapes') {
-    insertImage = divGrapes;
-} else if (product === 'raspberry') {
-    insertImage = divRaspberry;
-} else {
-    insertImage = divCoconut;
-};   
-
-document.body.append(insertImage);
 document.write(`
     <div class="product" align="center">
+    ${productImg}
     <p>Selected product: <b>${product}</b></p>
     <p>Count of ${category}: <b>${count}</b></p>
     <p>Selected period: <b>${season}</b></p>
